ea88e1689b13461f32e3b0fdaecb2751f465185c
Giservin <85154847+Giservin@users.noreply.github.com>
2022-10-30 16:40:30 +0700

    added error handling

--- app.py
+++ app.py
@@ -10,12 +10,18 @@ access_token_secret = os.environ['ACCESS_TOKEN_SECRET']
 authenticator = tweepy.OAuthHandler(api_key, api_key_secret)
 authenticator.set_access_token(access_token, access_token_secret)
 
-api = tweepy.API(authenticator, wait_on_rate_limit=True)
+try:
+	api = tweepy.API(authenticator, wait_on_rate_limit=True)
+except FailToConnect:
+	print('Failed to connect')
 
 while True:
 	file = open('count.txt', 'r')
 	day = int(file.read())
-	api.update_status('Day '+ str(day)+ ' : No')
+	try:
+		api.update_status('Day '+ str(day)+ ' : No')
+	except FailToConnect:
+		print('Failed to connect')
 	file = open('count.txt', 'w')
 	day = day+1
 	file.write(str(day))
